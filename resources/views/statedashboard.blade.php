c5b1e1d7706e06d6f96fbcfb7a6d844e83c78cfb
ridipgratia <ridipgoswami596@gmail.com>
2024-03-11 13:15:59 +0530

    add controller state view

--- resources/views/statedashboard.blade.php
+++ resources/views/statedashboard.blade.php
@@ -1,7 +1,25 @@
 <x-app-layout>
 @section('extracss')
+
     <!--- put extra css here -->
 @endsection
+{{-- <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+    </ul>
+  </div> --}}
+
+  <div class="d-flex border col-12 justify-content-center">
+<a href="djkkj">Show Data</a>
+  </div>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="row justify-content-center">
